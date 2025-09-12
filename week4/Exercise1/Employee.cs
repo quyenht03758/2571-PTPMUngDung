e184c88bdd613501c503d5cb977d9e1ed84a6754
Quyen Hoang <quyenht03758>
2025-09-12 23:47:53 +0700

    Finish practice week 4

---
+++ week4/Exercise1/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeManager
+{
+    public class Employee
+    {
+        public string Id { get; set; }          // Employee ID (unique)
+        public string Name { get; set; }        // Full name
+        public string Address { get; set; }     // Address
+        public DateTime Birthday { get; set; }  // Date of birth
+        public string Email { get; set; }       // Email
+        public string Phone { get; set; }       // Phone number
+    }
+}
